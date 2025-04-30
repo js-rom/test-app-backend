df6abdfea3a466190c3f409565e9381e42815038
[jesus romero] <jesus.romerovidal@gmail.com>
2025-04-30 17:42:29 +0200

    initial commit

---
+++ src/test/java/com/js_rom/test_app_backend/TestAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js_rom.test_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
