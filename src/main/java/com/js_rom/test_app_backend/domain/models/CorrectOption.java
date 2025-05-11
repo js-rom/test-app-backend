b26b5f92c31d8f060a9fac1781db21574c15b9ea
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-11 20:07:48 +0200

    enhance Option Model #3

--- src/main/java/com/js_rom/test_app_backend/domain/models/CorrectOption.java
+++ src/main/java/com/js_rom/test_app_backend/domain/models/CorrectOption.java
@@ -2,18 +2,19 @@ package com.js_rom.test_app_backend.domain.models;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class CorrectOption extends Option {
 
-    private boolean isCorrect = true;
+    CorrectOption() {
+        super.setCorrect(true);
+    }
 
     CorrectOption(String id, String description) {
         super(id, description);
+        super.setCorrect(true);
     }
 }
