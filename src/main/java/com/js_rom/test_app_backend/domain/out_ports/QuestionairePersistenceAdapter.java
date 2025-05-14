23f007d9ba4bd3ec4c8de2fe8f1714e7aea0329f
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-14 23:09:19 +0200

    add create questionaire to domain #1

--- src/main/java/com/js_rom/test_app_backend/domain/out_ports/QuestionairePersistenceAdapter.java
+++ src/main/java/com/js_rom/test_app_backend/domain/out_ports/QuestionairePersistenceAdapter.java
@@ -1,5 +1,8 @@
 package com.js_rom.test_app_backend.domain.out_ports;
 
+import com.js_rom.test_app_backend.domain.models.Questionaire;
+
 public interface QuestionairePersistenceAdapter {
 
+    public Questionaire create(Questionaire questionaire);
 }
--- src/main/java/com/js_rom/test_app_backend/domain/services/QuestionaireService.java
+++ src/main/java/com/js_rom/test_app_backend/domain/services/QuestionaireService.java
@@ -1,5 +1,20 @@
 package com.js_rom.test_app_backend.domain.services;
 
-public class QuestionaireService {
+import com.js_rom.test_app_backend.domain.in_ports.QuestionaireServiceAdapter;
+import com.js_rom.test_app_backend.domain.models.Questionaire;
+import com.js_rom.test_app_backend.domain.out_ports.QuestionairePersistenceAdapter;
+
+public class QuestionaireService implements QuestionaireServiceAdapter {
+
+    QuestionairePersistenceAdapter questionairePersistence;
+
+    public QuestionaireService(QuestionairePersistenceAdapter questionairePersistence){
+        this.questionairePersistence = questionairePersistence;
+    }
+
+    @Override
+    public Questionaire create(Questionaire questionaire) {
+       return this.questionairePersistence.create(questionaire);
+    }
 
 }
